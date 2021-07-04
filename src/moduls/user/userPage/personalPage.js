dc9449f9c9bd44f2baf9d125a9cb96d55443640b
OktayCopurlu <selimhalim12@gmail.com>
2021-07-05 00:32:50 +0200

    m

--- src/moduls/user/userPage/personalPage.js
+++ src/moduls/user/userPage/personalPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./personalPage.css";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -6,28 +6,28 @@ import MainModal from "../editUser/mainModal";
 import * as userService from "../../../services/users-service";
 import UserDetailCard from "./userDetailCard";
 export default function PersonalPage() {
-
   const [userMetadata, setUserMetadata] = useState(null);
   const { user, getAccessTokenSilently } = useAuth0();
-  const userId = user.sub;
-  
   
   useEffect(() => {
-    userService
-      .getUserMetadata(userId, getAccessTokenSilently)
+    const userId =  user.sub;
+    userService.getUserMetadata(userId, getAccessTokenSilently)
       .then((data) => {
         setUserMetadata(data);
       });
-  }, [userId]);
-
+  },[]);
   return (
     <>
       <div className="out-container">
         <UserDetailCard element={userMetadata} />
         <MainModal />
       </div>
-      <Link to="/my-products" className=" text-info"><h3 className="text-center p-0">My Products </h3></Link>
-      <Link to="/my-wishes" className=" text-info"><h3 className="text-center p-0">My Wishes</h3></Link> 
+      <Link to="/my-products" className=" text-info">
+        <h3 className="text-center p-0">My Products </h3>
+      </Link>
+      <Link to="/my-wishes" className=" text-info">
+        <h3 className="text-center p-0">My Wishes</h3>
+      </Link>
     </>
   );
 }
