fc2b6db2db2cea351d85c3e8cc5bc393269f810b
OktayCopurlu <selimhalim12@gmail.com>
2021-10-10 16:36:32 +0200

    added read more and less

--- src/modules/home/texts/whatIsFreeMarketText.js
+++ src/modules/home/texts/whatIsFreeMarketText.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export default function WhatIsFreeMarketText() {
-const {t} = useTranslation()
+  const { t } = useTranslation();
+  const [readMore, setReadMore] = useState(true);
 
+  const openText = () => {
+    setReadMore(false);
+  };
+  const closeText = () => {
+    setReadMore(true);
+  };
   return (
     <div>
-      <p className="mt-4 information-text">
-        {t('WhatIsFreeMarketText.1')}
-      </p>
-      <p className="mt-4 information-text">
-      {t('WhatIsFreeMarketText.2')}
-      </p>
-      <p className="mt-4 information-text">
-      {t('WhatIsFreeMarketText.3')}
-      </p>
+      <p className="mt-4 information-text">{t("WhatIsFreeMarketText.1")}</p>
+      {readMore ? (
+        <Link
+          className="text-decoration-none text-success h5"
+          onClick={openText}
+        >
+          read more
+        </Link>
+      ) : (
+        <>
+          <p className="mt-4 information-text">{t("WhatIsFreeMarketText.2")}</p>
+          <p className="mt-4 information-text">{t("WhatIsFreeMarketText.3")}</p>
+
+          <Link
+            className="text-decoration-none text-danger h5"
+            onClick={closeText}
+          >
+            read less
+          </Link>
+        </>
+      )}
     </div>
   );
 }
